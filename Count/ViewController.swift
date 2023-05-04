70e98b931ff6f373d7e9ed76c5e54cfa306bc83e
kakerusuzuki09tyoby@gmail.com <kakerusuzuki09tyoby@gmail.com>
2023-05-04 13:57:40 +0900

    Initial Commit

---
+++ Count/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Count
+//
+//  Created by 森掛 on 2023/05/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
